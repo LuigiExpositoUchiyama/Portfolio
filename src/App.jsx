c9070bcf01944210ee01c2a37d8bab7440313fcc
Luigi Exposito Uchiyama <luigi_uchiyama@outlook.com>
2025-01-10 15:29:14 -0300

    arrumando vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.module.css';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (window.location.pathname !== '/') {
-      navigate('/');
-    }
-  }, [navigate]);
-
   return (
     <>
       <Header />
