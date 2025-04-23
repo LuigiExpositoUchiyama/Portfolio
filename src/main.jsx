8c47c550c8522f532b7233be5ac1ed52bfbdfb6c
Luigi Exposito Uchiyama <luigi_uchiyama@outlook.com>
2025-04-23 00:51:32 -0300

    otimizacao

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,52 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// Importação dos componentes com lazy loading
-const App = lazy(() => import('./App.jsx'));
-const Home = lazy(() => import('./Routes/Home'));
-const Portfolio = lazy(() => import('./Routes/Portfolio'));
-const Contact = lazy(() => import('./Routes/Contact'));
-const Experience = lazy(() => import('./Routes/Experiences'));
-const Education = lazy(() => import('./Routes/Educations'));
-const Certificates = lazy(() => import('./Routes/Certificates'));
-const Error = lazy(() => import('./Routes/Error'));
+import App from './App.jsx';
+import Home from './Routes/Home';
+import Portfolio from './Routes/Portfolio';
+import Contact from './Routes/Contact';
+import Experience from './Routes/Experiences';
+import Education from './Routes/Educations';
+import Certificates from './Routes/Certificates';
+import Error from './Routes/Error';
 
-// Definindo as rotas
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <Error />,
     children: [
-      { index: true, element: <Home /> },
-      { path: 'portfolio', element: <Portfolio /> },
-      { path: 'contact', element: <Contact /> },
-      { path: 'experience', element: <Experience /> },
-      { path: 'education', element: <Education /> },
-      { path: 'certificates', element: <Certificates /> },
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'experience',
+        element: <Experience />,
+      },
+      {
+        path: 'education',
+        element: <Education />,
+      },
+      {
+        path: 'certificates',
+        element: <Certificates />,
+      },
     ],
   },
 ]);
 
-// Renderização com Suspense global
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense fallback={<div>Carregando...</div>}>
-      <RouterProvider router={router} />
-    </Suspense>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
